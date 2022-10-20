bcfd5b805c87a29fcbeef0c6355d37278a60e390
Kosma Pajor <110028823+kosmaelpe@users.noreply.github.com>
2022-10-20 12:57:49 +0200

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,13 @@
+let open = document.getElementById("open");
+let close = document.getElementById("close");
+let modal_container = document.getElementById("modal_container");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
+
+console.log("js dziala");
